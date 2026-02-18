d9f8d91407e7a619f1ac813ee983cfeb9d07defd
Olga Zaiets <olga.zayets@gmail.com>
2026-02-18 16:24:42 +0100

    added middleware cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,7 +24,10 @@ app.use(
 );
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://e-commerce-dusky-alpha-32.vercel.app'
+    ],
     credentials: true,
   })
 );
